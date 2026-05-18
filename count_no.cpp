caf41bbe08e6fb2fc6e4547845f8a5de7e348ae3
Abhishek-k1 <abhishek97766@gmail.com>
2026-05-18 19:25:33 +0530

    updated

--- count_no.cpp
+++ count_no.cpp
@@ -1,29 +1,48 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
-    long long int n;   // store input number   
-    int count = 0;  // count  total digits
+    int n;         // it print upto 10 digit if we have use long long int then print upto 19  if further want to count then use string
+    int count = 0; // count  total digits
 
     // take number from user
-    cout << "Enter a number: ";    
-    cin >>  n;
+    cout << "Enter a number: ";
+    cin >> n;
 
     // loop  runs until number becomes 0
-   while(n > 0)
-   {
-    // increase digit count
-    count++;
+    while (n > 0)
+    {
+        // increase digit count
+        count++;
 
-    //romove last digit
-  n = n / 10;
-   }
+        // romove last digit
+        n = n / 10;
+    }
 
-   // print  total digits
-   cout << "Total digits: " << count;
+    // print  total digits
+    cout << "Total digits: " << count;
 
     return 0;
 }
 
+/*
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+      string n;
+
+      cout << "Enter a number: ";
+      cin >> n;
+
+      cout << "Total digits: " << n.length();
+
+      return 0;
+
+}
+*/
