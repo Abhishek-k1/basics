70023ebf8896655310ae3e71f5cf4b7a023f9221
Abhishek-k1 <abhishek97766@gmail.com>
2026-05-18 21:32:24 +0530

    Add files via upload

--- sec_lar.cpp
+++ sec_lar.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-
-int n, largest, second;
-
-cout << "Enter size of array: ";
-cin >> n;
-
-int arr[n];
-cout <<  "Enter array element: ";
-
-
-
-for (int i = 0; i<n; i ++){
-cin >> arr[i];
-}
-largest = arr[0];
-second = -1;
-
-for(int i = 0; i < n; i++)
-if(arr [i] > largest){
-second = largest;
-largest = arr[i];
-}
-else if(arr[i] > second && arr[i] != largest)
+int main()
 {
-    second = arr[i];
-}
-    cout << second;
+
+    int n, largest, second;    // variable declaration
+
+    cout << "Enter size of array: ";   // take size  of array
+    cin >> n;
+
+    int arr[n];           // create array
+    cout << "Enter array element: ";    // take array elements
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    largest = arr[0];          // assume first element is largest
+
+    second = -1;              // Initially second largest is very small
+
+    for (int i = 0; i < n; i++)  // loop for comparison
+    {
+        if (arr[i] > largest)   // check  if current element is bigger than largest
+        {
+            second = largest; // old largest becomes second largest
+
+            largest = arr[i];   // update largest
+        }
+        else if (arr[i] > second && arr[i] != largest)  // check  for second largest
+        {
+            second = arr[i];   // update second largest
+        }
+    }
+    cout << "second largest element is: " << second;  // print second largest  element
 
     return 0;
-
 }
