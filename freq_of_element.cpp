dca49c11044895a11c96dfd1be7b484027660edc
Abhishek-k1 <abhishek97766@gmail.com>
2026-05-20 19:30:03 +0530

    updated

--- freq_of_element.cpp
+++ freq_of_element.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-
-    int n, count = 0;
-     cout << "Enter size of array: ";
-     cin >> n;
+int main()
+{
+    int n, count;
+    cout << "Enter size of array: ";
+    cin >> n;
 
     int arr[n];
     cout << "Enter array elements: ";
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-    cin  >> arr[i];
+        cin >> arr[i]; // store elements in array
     }
 
-    for(int i = 0; i < n; i++)
+    // outer loop selects one element
+    for (int i = 0; i < n; i++)
     {
         count = 0;
-    for(int j = 0; j < n; j++)
-    {
-        if(arr[i] == arr[j])
+
+        // Inner loop compares selected element with all elements
+        for (int j = 0; j < n; j++)
         {
-        count++;
+            if (arr[i] == arr[j]) // check if element are same
+            {
+                count++; // Increase frequency count
+            }
+        }
+
+        // print frequency of current element
+        cout << arr[i] << " appears " << count << " times" << endl;
     }
-}
-    cout << arr[i] << " appears " << count << " times" << endl;;
-}
-return 0;
+    return 0;
 }
