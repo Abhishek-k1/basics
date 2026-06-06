bc209d73f6612563744d029c9701dbe722528a1f
Abhishek-k1 <abhishek97766@gmail.com>
2026-06-06 21:31:55 +0530

    Add files via upload

---
+++ dsa.cpp
@@ -0,0 +1,61 @@
+/*                        DSA
+Introduction-> Data structure + algorithms
+Data structure--> waay of storing data like
+array
+linkedlist
+staack
+queue
+tree
+graph
+Algorithm-> step by step procedure to solve a problem 
+ex-> finding largesst number in a array
+
+Important of DSA
+makes prograam faster
+uses lesss memory
+immproves problem solvinng skills
+help for coding interview
+
+TIME COMPLEXITY
+time complexity tells how much work an algorithm performs as input size increase
+it does not measure seconds
+it measure number of operation
+Ex->
+ for(int i = 0; i < n; i++)
+ {
+ cout << i;
+ }
+
+ Loop runs n times
+ Time Complexity = 0(n)
+
+BIG O NOTATION
+big o represents worst case complexity
+some common complexity->
+O(1)-> Constant Time
+O(log n)-> Logarithmic Time
+O(n)-> Linear time
+O(n log n)-> Efficient sorting
+O(n2)-> Nested Loops
+
+Asymptotic Notations
+1.Big O(O)-> Worst Case
+2.Big Omega(Ω)-> Best case
+3. Big theta(Θ)-> Average/ Tight Bound
+
+Example-> Linear Search
+Array:- 10 20 30 40 50
+IF searching 10: found immediately 
+                 Best Case Ω(1)
+IF searching 50: worst case O(n)
+                 because check every element
+If searching random element: Average Case Θ(n)
+
+
+
+
+
+
+
+
+*/
