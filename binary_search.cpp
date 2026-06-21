823970ea98c054499eca91f0b02ca2a6d772e16c
Abhishek-k1 <abhishek97766@gmail.com>
2026-06-21 22:04:41 +0530

    binary search program

--- binary_search.cpp
+++ binary_search.cpp
@@ -73,3 +73,76 @@ int main() {
    }
    return 0;
 }
+
+
+/*----------------------------------------------- Binary search ----------------------------------------------------------
+
+Binary search repeatedly divides the search space into two halves until the target element found or the search space become empty
+
+Binary search works only on a sorted array
+Example:-
+Array: 1 4 5 7 9
+Target: 5
+output:-
+Element found at index: 2
+
+This program demonstrates:-
+1. Array
+2. Loops
+3. Conditional Statements
+4. Boolean Variable
+5. Searching Algorithms
+6. Divide and conquer Technique
+
+Algorithm:-
+1. Read the size of the array
+2. Input the sorted array elements
+3. Input the target element
+4. Initialize
+    low = 0;
+    high = n - 1;
+5. While low <= high
+Find middle index:
+   mid = (low + high) / 2;
+If arr[mid] == target, element is found
+If arr[mid] < target, search the right half
+   low = mid + 1;
+otherwise search the left half
+   high = mid - 1;
+6. If the loop ends and element is not found, print"Element not found".
+
+
+Working flow:-
+START
+↓
+Input sorted array
+↓
+Input target element
+↓
+low = 0
+high = n - 1
+↓
+low <= high ?
+↓ NO
+Element not found
+↓ YES
+Find mid
+↓
+arr[mid] == target ?
+↓ YES
+Element found
+↓ NO
+arr[mid] < target ?
+↓ YES
+low = mid + 1
+↓ NO
+high = mid - 1
+↓
+Repeat
+↓
+END
+
+Time complexity:- O(log n)
+Space complexity:-O(1)
+
+*/
