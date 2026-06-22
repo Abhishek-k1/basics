00b8b395e324a5283d11907e70b6f169137e070e
Abhishek-k1 <abhishek97766@gmail.com>
2026-06-22 19:54:59 +0530

    Count even and odd no in array

--- count_even_odd.cpp
+++ count_even_odd.cpp
@@ -5,7 +5,7 @@ int main() {
 
     int n;
 
-    cout << "Enter size of array:";
+    cout << "Enter size of array: ";
     cin >> n;
     
     int arr[n];
@@ -41,3 +41,50 @@ int main() {
     
     return 0;
 }
+
+/*--------------------------------------- Count Even and Odd Numbers in an array ------------------------------------------------
+An even number is divisible by 2 whereas an odd number is not divisible by 2
+
+Algorithm:
+1. Read the size of the array
+2. Input all array elements
+3. Initialize:
+    even = 0;
+    odd = 0;
+4. Traverse the array from index 0 to n-1
+5. For each element
+    if arr[i] % 2 == 0
+      increment even count
+    otherwise
+      increment odd count
+6. Print the total even count
+7. Print the total odd count
+
+Working:
+
+START
+↓
+Input array size
+↓
+Input array elements
+↓
+Initialize
+even = 0
+odd = 0
+↓
+Traverse array
+↓
+arr[i] % 2 == 0 ?
+↓ YES
+even++
+↓ NO
+odd++
+↓
+Repeat for all elements
+↓
+Print even count
+↓
+Print odd count
+↓
+END
+*/
