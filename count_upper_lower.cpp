400710e0cb7ce66c60b1348043f284852e6ab415
Abhishek-k1 <abhishek97766@gmail.com>
2026-06-24 21:33:18 +0530

    count upper and lowercase character

--- count_upper_lower.cpp
+++ count_upper_lower.cpp
@@ -31,3 +31,52 @@ int main()
 
      return 0;
 }
+/*------------------------------------------------ Count Uppercase and Lowercase Character ---------------------------------------------------------------
+
+To count upper case and lower case 
+
+Algorithm:-
+1. Read the input string
+2. Initialize
+    upper = 0
+    lower = 0
+3. Traverse the string from index 0 to length 1
+4. For each character:
+    -> if character is between 'A' and 'Z'
+       increment upper
+    -> if character is between 'a' and 'z'
+       increment lower
+5. print uppercase count
+6. print lowercase count
+
+
+
+Working flow:-
+START
+↓
+Input string
+↓
+upper = 0
+lower = 0
+↓
+Traverse string
+↓
+Character between A-Z ?
+↓ YES
+upper++
+↓
+Character between a-z ?
+↓ YES
+lower++
+↓
+Repeat for all characters
+↓
+Print upper count
+↓
+Print lower count
+↓
+END
+
+
+
+*/
