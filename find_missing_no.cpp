21275f6e4fe12ef0ea6b41eac11995943051c105
Abhishek-k1 <abhishek97766@gmail.com>
2026-06-29 21:10:53 +0530

    missing number

--- find_missing_no.cpp
+++ find_missing_no.cpp
@@ -36,3 +36,53 @@ int main()
 
     return 0;
 }
+/*------------------------------------------------------------- MIssing nnumber -----------------------------------------------
+A missing number is a number that is absent from sequence 1 to n
+Example:-
+Input:
+n = 5
+Array: 1 2 4 5
+Output:
+Missing Number = 3
+
+This program demonstrate:-
+1. Array
+2. Loops
+3. Arithmetic operators
+4. Mathematical Formula
+5. Accumulation
+6. Optimization Technique
+
+Algorithm:-
+1. Read the value of n
+2. Create an array of size n-1
+3. Input all array elements
+4. Calculate the actual sum of the array elements
+5. Calculate the ecpected sum of numbers  from 1 to n using:
+         expected = n * (n  + 1) / 2
+6. Find the  missing number:
+        missing = expected - sum
+7. Print the missing number
+
+
+Working flow:-
+START
+↓
+Input n
+↓
+Input array elements
+↓
+Calculate actual sum
+↓
+Calculate expected sum
+↓
+missing = expected - actual sum
+↓
+Print missing number
+↓
+END
+
+
+
+
+*/
