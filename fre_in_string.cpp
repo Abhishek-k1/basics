59c4c2813c5b9070d310adc1e86ebb0ef81fe93b
Abhishek-k1 <abhishek97766@gmail.com>
2026-07-05 21:19:16 +0530

    frequency using string

--- fre_in_string.cpp
+++ fre_in_string.cpp
@@ -28,3 +28,63 @@ int main() {
 }
 return 0;
 }
+
+
+/*--------------------------------------------------------- Count frequency using string ------------------------------------------------------
+
+The frequency of a chaaracter is the number of times it appears in a string
+
+Example:
+
+Input:
+apple
+
+Output:
+a = 1
+p = 2
+p = 2
+l = 1
+e = 1
+
+
+This program demonstrate:-
+1. string-> used to store the input string
+2. Nested Loops-> used to compare each character with every other character
+3. Conditional statements-> used to check whether two characters are equal
+4. String Traversal-> used to visit every character of the string
+5. Frequency Counting->  used to counnt how many times each character appears
+6. Brute Force Technique-> used to calculate the frequency by comparing every possible pair of character
+
+
+Algorithm:-
+1. Read the input string
+2. Traverse the string using an outer  loop
+3. Initialize count = 0;
+4. Traverse the string again using an inner loop
+5. Compare the selected character with every character
+6. If both character are equal, increment count
+7. print the character and its frequency
+8. repeat for all characters
+
+Working flow:-
+START
+↓
+Input string
+↓
+Select one character
+↓
+Set count = 0
+↓
+Compare with every character
+↓
+Characters equal?
+↓ YES
+Increment count
+↓
+Print character and frequency
+↓
+Repeat for all characters
+↓
+END
+
+*/
